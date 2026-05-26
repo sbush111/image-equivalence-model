0c7578ba1755bf6461346bf1b2e672f7c4121322
Sean Bush <sbus139@wgu.edu>
2026-05-26 10:10:57 -0500

    Pass model params directly into constructor

--- model.py
+++ model.py
@@ -2,81 +2,69 @@ from config import Config
 import torch
 from torch import nn, Tensor
 from torch.nn import functional as F
-from typing import Self
+from typing import Optional, Self
 
 class ImagePairMatcher(nn.Module):
 
-    def __init__(self, config: Config):
+    def __init__(self, 
+                 cnn_hidden_layers: list[int],
+                 fc_hidden_layers: list[int],
+                 cnn_dropout: float = 0.0, 
+                 fc_dropout: float = 0.0):
 
         super().__init__()
 
-        self.cnn_block1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1), # 3 x 32 x 32 -> 16 x 32 x 32
-            nn.BatchNorm2d(num_features=16),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2) # 16 x 32 x 32 -> 16 x 16 x 16
-        )
+        cnn_blocks = []
+        cnn_hidden_layers = [3] + cnn_hidden_layers
+        for layer_in, layer_out in zip(cnn_hidden_layers[:-1], cnn_hidden_layers[1:]):
+            cnn_block = nn.Sequential(
+                nn.Conv2d(in_channels=layer_in, out_channels = layer_out, kernel_size=3, padding=1),
+                nn.BatchNorm2d(num_features=layer_out),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.Dropout2d(cnn_dropout)
+            )
+            cnn_blocks.append(cnn_block)
+        self.cnn = nn.Sequential(*cnn_blocks)
+        
+        self.gap = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.flat = nn.Flatten()
 
-        self.cnn_block2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1), # 16 x 16 x 16 -> 32 x 16 x 16
-            nn.BatchNorm2d(num_features=32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2), # 32 x 8 x 8
-            nn.Dropout2d(0.2)
-        )
+        fc_blocks = []
+        fc_hidden_layers = [4 * cnn_hidden_layers[-1]] + fc_hidden_layers
+        for layer_in, layer_out in zip(fc_hidden_layers[:-1], fc_hidden_layers[1:]):
+            fc_block = nn.Sequential(
+                nn.Linear(layer_in, layer_out),
+                nn.ReLU(),
+                nn.Dropout(fc_dropout)
+            )
+            fc_blocks.append(fc_block)
+        self.fc = nn.Sequential(*fc_blocks)
 
-        self.cnn_block3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1), #32 x 8 x 8 -> 64 x 8 x 8
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2) # 64 x 8 x 8 -> 64 x 4 x 4
-        )
-
-        self.gap = nn.AdaptiveAvgPool2d(output_size=(1, 1)) # 64 x 4 x 4 -> 64 x 1 x 1
-        self.flat = nn.Flatten() # 64 x 1 x 1 -> 64
-
-        self.fc1 = nn.Sequential(
-            nn.Linear(4 * 64, 32),
-            nn.ReLU(),
-            nn.Dropout(0.2)
-        )
-
-        self.fc2 = nn.Sequential(
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Dropout(0.2)
-        )
-
-        self.out = nn.Linear(16, 1)
+        self.out = nn.Linear(fc_hidden_layers[-1], 1)
 
     def _embed(self, x: Tensor) -> Tensor:
-        x = self.cnn_block1(x)
-        x = self.cnn_block2(x)
-        x = self.cnn_block3(x)
+        x = self.cnn(x)
         x = self.gap(x)
         return self.flat(x)
 
     def forward(self, x1: Tensor, x2: Tensor) -> Tensor:
         features1 = self._embed(x1)
         features2 = self._embed(x2)
-        difference = features1 - features2
-        difference = torch.abs(difference)
+        difference = torch.abs(features1 - features2)
         hadamard = features1 * features2
-        x = torch.concat([features1, features2, difference, hadamard], dim=-1)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return self.out(x)
+        x = torch.cat([features1, features2, difference, hadamard], dim=-1)
+        x = self.fc(x)
+        return self.out(x).squeeze(-1)
 
-    def predict(self, x1: Tensor, x2: Tensor, threshold: float = 0.5) -> bool:
-        return F.sigmoid(self.forward(x1, x2)) >= threshold
-
-    @staticmethod
-    def from_config(config: Config) -> Self:
-        model = ImagePairMatcher(config) # TODO...
-        return model
+    @torch.no_grad()
+    def predict(self, x1: Tensor, x2: Tensor, threshold: float = 0.5) -> Tensor:
+        return torch.sigmoid(self.forward(x1, x2)) >= threshold
 
 '''
 TODO:
-Turn magic values into variables
-Populate from_config with code to set variables based on config values
+    @staticmethod
+    def from_config(config: Config) -> Self:
+        model = ImagePairMatcher(config) #TODO
+        return model
 '''

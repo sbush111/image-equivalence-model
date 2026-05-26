28194a0929f5cb921fd61b17a12f7b68c519535a
Sean Bush <sbus139@wgu.edu>
2026-05-26 16:02:41 -0500

    Create general purpose training loop

---
+++ train.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+import torch
+from torch.nn import Module
+from torch import device as Device
+from torch.nn.modules.loss import _Loss as Loss
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from typing import Optional
+
+@dataclass
+class TrainResults:
+    train_losses: list[float]
+    validate_losses: Optional[list[float]]
+    
+def train(model: Module, 
+          criterion: Loss, 
+          optimizer: Optimizer,
+          num_epochs: int,
+          train_loader: DataLoader,  
+          validate_loader: Optional[DataLoader] = None,
+          device: Optional[Device] = None,
+          progress: bool = False) -> TrainResults:
+
+    if device is None:
+        device = Device('cpu')
+
+    model = model.to(device)
+
+    train_losses = []
+    validate_losses = [] if validate_loader is not None else None
+
+    iterator = range(num_epochs)
+    if progress:
+        iterator = tqdm(iterator)
+    
+    for epoch in iterator:
+
+        train_loss = 0.0
+        model.train()
+
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            batch_size = inputs.size(0)
+            train_loss += loss.item() * batch_size
+
+        train_losses.append(train_loss / len(train_loader.dataset))
+
+        if validate_loader is None:
+            continue
+
+        validate_loss = 0.0
+        model.eval()
+
+        with torch.no_grad():
+
+            for inputs, labels in validate_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                batch_size = inputs.size(0)
+                validate_loss += loss.item() * batch_size
+
+        validate_losses.append(validate_loss / len(validate_loader.dataset))
+
+    return TrainResults(train_losses, validate_losses)
